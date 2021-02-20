daca66d4a9e0d4b32dbf3431690453260b103007
andreistoc <andreistoc@gmail.com>
2021-02-20 15:42:22 +0200

    Initial Commit

---
+++ OnboardingApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnboardingApp
+//
+//  Created by Andrei Istoc on 20.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OnboardingApp/OnboardingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingAppApp.swift
+//  OnboardingApp
+//
+//  Created by Andrei Istoc on 20.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
